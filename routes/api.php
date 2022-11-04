c9597816a3986684cfd59e0866e09c54c8512321
castagh <serkahniskala@gmail.com>
2022-11-04 14:07:05 +0700

    Migration & Auth sori digabung pak :)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('me', [AuthController::class, 'me']);
+
+Route::get('author', [AuthController::class, 'author']);
+
+Route::resource('books', BookController::class)->except([
+    'create', 'edit'
+]);
+
+Route::resource('authors', AuthorController::class)->except([
+    'create', 'edit'
+]);
+
+//protected route
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
 });
